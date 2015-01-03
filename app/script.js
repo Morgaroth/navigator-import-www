f05cccd3a81293c364f138932fbc2df030e808c4
Mateusz Jaje <mateuszjaje@gmail.com>
2015-01-03 21:24:49 +0100

    updated link to home.html

--- app/script.js
+++ app/script.js
@@ -4,7 +4,7 @@ var app = angular.module('single-page-app', ['ngRoute']);
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: 'app/home.html'
+            templateUrl: 'home.html'
         });
     //.when('/about', {
     //    templateUrl: 'about.html'
